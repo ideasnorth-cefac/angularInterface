cc9622abeee2075bad34b6ceae2f54ab987bbee8
ideasnorth-cefac <75029356+ideasnorth-cefac@users.noreply.github.com>
2020-11-25 18:10:43 +0100

    Add files via upload

---
+++ src/app/components/posts/post.interface.ts
@@ -0,0 +1,10 @@
+export interface post_interface
+{
+    // id:number;
+    url: Array<String>;
+    // title:string;
+    // content: string;
+    // image: string;
+    // date: string;
+    // status?: string;
+}
